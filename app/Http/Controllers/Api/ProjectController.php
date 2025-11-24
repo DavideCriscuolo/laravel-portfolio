6dd93d63bbf8ead821e33f7f9342ad1f4df25fff
DavideCriscuolo <davide.alcatel@gmail.com>
2025-11-24 19:07:05 +0100

    creata api index che mostra i progetti con il tipo

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public 
+    public function index()
+    {
+
+        $projects = Project::with("type")->get();
+        return response()->json([
+            "Sucess" => true,
+            "data" => $projects
+        ]);
+    }
 }
